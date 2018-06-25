a4b24e8deec8ef7c97040a727dca71286fecc83d
rdnraj99 <rdnraj@hotmail.com>
2018-06-26 00:20:49 +0530

    initial commit after JPA many to many

---
+++ src/main/java/com/rnehul/spring5artifact/Spring5artifactApplication.java
@@ -0,0 +1,14 @@
+package com.rnehul.spring5artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5artifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5artifactApplication.class, args);
+		System.out.println("Application Started");
+
+	}
+}
---
+++ src/main/java/com/rnehul/spring5artifact/model/Author.java
@@ -0,0 +1,93 @@
+package com.rnehul.spring5artifact.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Rahul
+ */
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstname;
+    private String lastname;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book>books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Author(String firstname, String lastname, Set<Book> books) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.books = books;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+}
